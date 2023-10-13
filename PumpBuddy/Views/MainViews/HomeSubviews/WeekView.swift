8b2ebc34dc43f51e27e520cacf313a35222fbf2d
s3854412 <79833688+s3854412@users.noreply.github.com>
2023-10-13 11:59:18 +1100

    WeeklyView with coredata fetching

--- PumpBuddy/Views/MainViews/HomeSubviews/WeekView.swift
+++ PumpBuddy/Views/MainViews/HomeSubviews/WeekView.swift
@@ -4,59 +4,107 @@
 //
 //  Created by udit on 22/08/23.
 //
-
 import SwiftUI
+import CoreData
+
+
+struct DayDate: Identifiable {
+    var id: String { day }
+    let day: String
+    
+    var formattedDate: String {
+        return day
+    }
+}
 
 struct WeekView: View {
     @Environment(\.colorScheme) var colorScheme
+    @State private var selectedDay: String?
+    
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: .default)
+    private var workouts: FetchedResults<Workout>
+    
     var body: some View {
-        
-        //This is a hard-coded display as of now as we do not have actual user data to load
-        //Logic for this will be done in the VM folder
-        VStack(alignment: .leading){
-            Text("This week's activity")
-                .padding([.leading, .bottom], 8.0)
-                .font(.custom("Arial", size: 25))
-                .bold()
-            HStack{
-                circleView(day:"M",done:true,date:Date())
-                circleView(day:"Tu",done:true,date:Date())
-                circleView(day:"W",done:false,date:Date())
-                circleView(day:"Th",done:true,date:Date())
-                circleView(day:"F",done:false,date:Date())
-                circleView(day:"Sa",done:false,date:Date())
-                circleView(day:"Su",done:false,date:Date())
+        NavigationView {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("This week's activity")
+                    .padding([.leading, .bottom], 8.0)
+                    .font(.custom("Arial", size: 25))
+                    .bold()
                 
+                let weekDays = getDaysInWeek()
+                
+                HStack {
+                    ForEach(weekDays) { dayInfo in
+                        let day = dayInfo.day
+                        let workoutForDay = fetchWorkoutForDay(day)
+                        
+                        if workoutForDay != nil {
+                            NavigationLink(
+                                destination: WorkoutDetailView(workout: workoutForDay!),
+                                isActive: Binding(
+                                    get: { selectedDay == day },
+                                    set: { _ in selectedDay = nil }
+                                ) // adapted from https://sarunw.com/posts/swiftui-button-disable/ and also used ChatGPT to help modify
+                            ) {
+                                CircleView(day: day, done: true, isSelected: selectedDay == day)
+                                    .onTapGesture {
+                                        selectedDay = day
+                                    } // adapted from https://sarunw.com/posts/swiftui-button-disable/ and also used ChatGPT to help modify
+                            }
+                        } else {
+                            CircleView(day: day, done: false, isSelected: selectedDay == day)
+                                .opacity(0.5) // adapted from https://sarunw.com/posts/swiftui-button-disable/ and also used ChatGPT to help modify
+                        }
+                    }
+                }
+                .foregroundColor(colorScheme == .light ? .white : .black)
             }
-            .foregroundColor(colorScheme == .light ? .white : .black)
+            .padding(.vertical, 16.0)
+            .frame(maxWidth: .infinity)
+            .background(Color("AppColor"))
+            .cornerRadius(20)
         }
-        .padding(.all)
-        .frame(maxWidth: .infinity)
-        .background(Color("AppColor"))
-        .cornerRadius(20)
+    }
+    
+    private func getDaysInWeek() -> [DayDate] {
+        return ["M", "T", "W", "T", "F", "S", "S"].map { DayDate(day: $0) }
+    }
+    
+    private func fetchWorkoutForDay(_ day: String) -> Workout? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E"
+        
+        return workouts.first { workout in
+            guard let workoutDate = workout.date else {
+                return false
+            }
+            let workoutDay = dateFormatter.string(from: workoutDate)
+            return workoutDay == day
+        }
     }
 }
 
-struct WeekView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeekView()
-    }
-}
-
-//Common view displayed for each day
-struct circleView: View{
+struct CircleView: View {
     @Environment(\.colorScheme) var colorScheme
     var day: String
     var done: Bool
-    var date: Date
+    var isSelected: Bool
+    
     var body: some View {
-        VStack{
+        VStack {
             Text(day)
+                .font(.subheadline)
+                .fontWeight(.bold)
+            
             ZStack {
                 Circle()
                     .frame(maxWidth: 40)
-                .foregroundColor(done == true ? .black : .white)
-                if done{
+                    .foregroundColor(isSelected ? .blue : (done ? .black : .white))
+                
+                if done {
                     Image(systemName: "checkmark")
                         .foregroundColor(.white)
                 }
@@ -64,3 +112,20 @@ struct circleView: View{
         }
     }
 }
+    
+    struct WorkoutDetailView: View {
+        var workout: Workout
+        
+        var body: some View {
+            VStack {
+                Text("Workouts for \(workout.date!, style: .date)")
+                    .font(.title)
+                
+                Text("Workout: \(workout.name ?? "")")
+                Text("Duration: \(workout.duration) minutes")
+                Text("Description: \(workout.describe ?? "")")
+            }
+        }
+    }
+    
+
