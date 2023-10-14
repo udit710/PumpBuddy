55cbf9041bbd434360866a17fa13b27bfd70941f
s3854412 <79833688+s3854412@users.noreply.github.com>
2023-10-14 20:53:11 +1100

    AccountPage and SignInPage Early Integration

--- PumpBuddy/Views/Login/SignUpPageView.swift
+++ PumpBuddy/Views/Login/SignUpPageView.swift
@@ -4,36 +4,107 @@
 //
 //  Created by udit on 22/08/23.
 //
-
 import SwiftUI
 
 struct SignUpPageView: View {
     @Environment(\.colorScheme) var colorScheme
     @AppStorage("username") var username: String = ""
-    @AppStorage("loggendIn") var loggedIn: Bool = false
-    @State private var isValid : Bool = false
+    @AppStorage("email") var email: String = ""
+    @AppStorage("password") var password: String = ""
+    @AppStorage("confirmedPassword") var confirmedPassword: String = ""
+    @AppStorage("currentWeight") var currentWeight: String = ""
+    @AppStorage("goalWeight") var goalWeight: String = ""
+    @AppStorage("loggedIn") var loggedIn: Bool = false
+    @State private var isValid: Bool = false
+    @State private var showInvalidInputAlert = false
 
     var body: some View {
-            VStack {
-                Form {
-                    HStack{
-                        TextField("Enter  username",text: $username)
-                    }
+        VStack(spacing: 20) {
+            Form {
+                Section(header: Text("Sign Up")) {
+                    InputField(placeholder: "Username", text: $username, keyboardType: .default)
+                    InputField(placeholder: "Email", text: $email, keyboardType: .emailAddress)
+                    SecureInputField(placeholder: "Password", text: $password)
+                    SecureInputField(placeholder: "Confirm Password", text: $confirmedPassword)
+                    NumericInputField(placeholder: "Current Weight", text: $currentWeight)
+                    NumericInputField(placeholder: "Goal Weight", text: $goalWeight)
                 }
-                Button("Continue"){
+            }
+
+            Button("Continue") {
+                if isValid {
                     loggedIn = true
+                    UserDefaults.standard.set(username, forKey: "username")
+                    UserDefaults.standard.set(email, forKey: "email")
+                    UserDefaults.standard.set(currentWeight, forKey: "currentWeight")
+                    UserDefaults.standard.set(goalWeight, forKey: "goalWeight")
+                } else {
+                    showInvalidInputAlert = true
                 }
-                .frame(width: 120, height: 35)
-                .background(colorScheme == .dark ? .white : .black)
-                .cornerRadius(25)
-                .foregroundColor(colorScheme == .dark ? .black : .white)
-                .font(.system(size: 20))
-                .bold()
-                .disabled(username.count < 4)
-                .opacity(username.count < 4 ? 0.5 : 1)
+            }
+            .buttonStyle(RoundedButtonStyle(isEnabled: isValid))
+        }
+        .padding()
+        .alert(isPresented: $showInvalidInputAlert) {
+            Alert(title: Text("Invalid Input"), message: Text("Please fill in all fields and ensure they meet the required criteria."), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct InputField: View {
+    var placeholder: String
+    @Binding var text: String
+    var keyboardType: UIKeyboardType
+
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .keyboardType(keyboardType)
+            .onChange(of: text) { _ in
+                // Perform input validation logic here
             }
     }
 }
+
+struct SecureInputField: View {
+    var placeholder: String
+    @Binding var text: String
+
+    var body: some View {
+        SecureField(placeholder, text: $text)
+            .onChange(of: text) { _ in
+                // Perform input validation logic here
+            }
+    }
+}
+
+struct NumericInputField: View {
+    var placeholder: String
+    @Binding var text: String
+
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .keyboardType(.numberPad)
+            .onChange(of: text) { _ in
+                // Perform input validation logic here
+            }
+    }
+}
+
+struct RoundedButtonStyle: ButtonStyle {
+    var isEnabled: Bool
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(isEnabled ? Color("AppColor") : Color.gray)
+            .cornerRadius(25)
+            .opacity(isEnabled ? 1 : 0.5)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+    }
+}
+
 
 struct SignUpPage_Previews: PreviewProvider {
     static var previews: some View {
--- PumpBuddy/Views/MainViews/AccountTabView.swift
+++ PumpBuddy/Views/MainViews/AccountTabView.swift
@@ -6,29 +6,70 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct AccountTabView: View {
+    @AppStorage("username") var username: String = ""
+    @AppStorage("email") var email: String = ""
+    @AppStorage("currentWeight") var currentWeight: String = ""
+    @AppStorage("goalWeight") var goalWeight: String = ""
+    @State private var selectedImage: UIImage? // To store the selected image
+    @State private var isImagePickerPresented: Bool = false
+
     var body: some View {
         NavigationView {
             VStack {
-                
                 Spacer()
-                HStack{
-                    Spacer()
-                    VStack{
-                        Spacer()
-                        NavigationLink(destination: WorkoutHistoryView()) {
-                            //Adapted and taken from https://sarunw.com/posts/swiftui-button-size/ to make button
-                            Image(systemName: "clock")
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(Color("AppColor"))
-                                .cornerRadius(20)
-                                .shadow(radius: 5)
-                                .padding()
+
+                VStack {
+                    Image(uiImage: selectedImage ?? UIImage(systemName: "person.fill")!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white, lineWidth: 2)
+                        )
+                        .onTapGesture {
+                            isImagePickerPresented = true
                         }
-                        .padding(.horizontal)
+                        .sheet(isPresented: $isImagePickerPresented) {
+                            ImagePicker(selectedImage: $selectedImage) {
+                                isImagePickerPresented = false
+                            }
+                        }
+                        .padding(.bottom)
+
+                    Text("Hello, \(username)!")
+                        .font(.title)
+                        .foregroundColor(.black)
+
+                    Text("Email: \(email)")
+                        .foregroundColor(.black)
+                        .font(.headline)
+                        .padding(.bottom)
+
+                    Text("Current Weight: \(currentWeight)")
+                        .foregroundColor(.black)
+                        .font(.headline)
+
+                    Text("Goal Weight: \(goalWeight)")
+                        .foregroundColor(.black)
+                        .font(.headline)
+                        .padding(.bottom)
+
+                    Spacer()
+
+                    NavigationLink(destination: WorkoutHistoryView()) {
+                        Image(systemName: "clock")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color("AppColor"))
+                            .cornerRadius(20)
+                            .shadow(radius: 5)
+                            .padding()
                     }
+                    .padding(.horizontal)
                     .navigationBarTitle("Account Info")
                     .toolbarBackground(
                         Color("AppColor"),
@@ -40,6 +81,46 @@ struct AccountTabView: View {
         }
     }
 }
+
+// ImagePicker View
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    var onDismiss: () -> Void // Callback to dismiss the sheet
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.editedImage] as? UIImage {
+                parent.selectedImage = selectedImage
+            } else if let selectedImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = selectedImage
+            }
+
+            parent.onDismiss()
+        }
+    }
+}
+
+
+
 
 struct AccountTabView_Previews: PreviewProvider {
     static var previews: some View {
