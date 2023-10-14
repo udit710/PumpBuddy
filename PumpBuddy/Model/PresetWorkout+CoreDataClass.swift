eedcca70f35c0b045ab8c8299ff090a9f185b238
udit710 <s3933905@student.rmit.edu.au>
2023-10-15 04:16:46 +1100

    Updated Model for preset exercises

---
+++ PumpBuddy/Model/PresetWorkout+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PresetWorkout+CoreDataClass.swift
+//  PumpBuddy
+//
+//  Created by udit on 15/10/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class PresetWorkout: NSManagedObject {
+
+}
