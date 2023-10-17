083161183f34043cb8bbc4ad3f0dbbde7403f145
udit710 <s3933905@student.rmit.edu.au>
2023-10-17 23:30:05 +1100

    Api Key saved to plists

--- PumpBuddy/Model/JsonModel.swift
+++ PumpBuddy/Model/JsonModel.swift
@@ -35,12 +35,26 @@ class JsonModel: ObservableObject{
         return exercises
     }
 
+    func getApiKey() -> String {
+        var key = ""
+
+        if let plistPath = Bundle.main.path(forResource: "Api_Key", ofType: "plist"),
+           let dict = NSDictionary(contentsOfFile: plistPath) as? [String: Any] {
+            if let apiKey = dict["Key"] as? String {
+                key = apiKey
+            }
+        }
+
+        return key
+    }
+
     
     func fetchData(dataNeeded: String, dataFor: String, context: NSManagedObjectContext) async throws -> [ExerciseModel] {
         let for_ = dataFor.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         let url = URL(string: "https://api.api-ninjas.com/v1/exercises?\(dataNeeded)=" + (for_ ?? ""))!
         var request = URLRequest(url: url)
-        request.addValue("0M/NVQZMsyAELTdiajCn1w==WPU2UGrGFcKcKLXm", forHTTPHeaderField: "X-Api-Key")
+        let key = getApiKey()
+        request.addValue(key, forHTTPHeaderField: "X-Api-Key")
         
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
